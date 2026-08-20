1e75fa72d04692ed631e35cd6e79f2a0f2ad2576
Clemilson Azevedo <clemilsondeazevedo@gmail.com>
2026-08-20 19:15:42 +0100

    add port env var

--- api/cmd/web/main.go
+++ api/cmd/web/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 
 	"github.com/clemilsonazevedo/look-news/cmd/api"
 )
 
 func main() {
-	if err := api.InitServer(); nil != err {
+	if err := api.InitServer(); err != nil {
+		slog.Error("Cannot init the server",
+			"error", err,
+		)
 		os.Exit(1)
 	}
 }
